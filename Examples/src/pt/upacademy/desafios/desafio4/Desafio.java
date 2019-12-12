17af6f79b2e41a0ca55386c9efff15414387006e
maurofpereira <maurofpereira@gmail.com>
2019-12-12 23:46:42 +0000

    No exercio 4, o scanner não funciona. No exercicio 5 o .ofPatter não funciona.

--- Examples/src/pt/upacademy/desafios/desafio4/Desafio.java
+++ Examples/src/pt/upacademy/desafios/desafio4/Desafio.java
@@ -1,13 +1,22 @@
 package pt.upacademy.desafios.desafio4;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+private static Scanner scanner;
+
 public class Desafio {
 
-	public static int[] ints(String string) {
-		
-
-		return string;
-		
-// coment teste sourec tree
+public static int[] ints(String msg) {
+	List<Integer> result = new ArrayList<Integer>();
+	
+	scanner =new Scanner(msg);
+	while (scanner.hasNext()) {
+		result.add(scanner.nextInt());
+	}else {
+		scanner.next();
 	}
-
+	return result.stream().mapToInt(i->i).toArray();
+}
 }
