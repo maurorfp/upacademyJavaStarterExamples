bffb980b50c2381a1cee259d015875d761e7282b
mauro <>
2019-12-16 19:19:02 +0000

    trabalho aulo

---
+++ Examples/src/io/altar/jseproject/repositories/ShelfRepository.java
@@ -0,0 +1,23 @@
+package io.altar.jseproject.repositories;
+
+import io.altar.jseproject.models.Shelf;
+
+public class ShelfRepository extends EntityRepository <Shelf> {
+	
+
+		private static final ShelfRepository INSTANCE= new ShelfRepository();
+		
+		
+
+		private ShelfRepository() {
+			
+		}
+		
+			
+		public static  ShelfRepository getInstance () {
+		
+		return INSTANCE;
+	
+	
+		}
+}
